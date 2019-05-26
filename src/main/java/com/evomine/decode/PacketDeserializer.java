fb661fe0b5e28da77a8dc03649aeefc4d5a18c81
Peyton Hammersley <voxelbee@gmail.com>
2019-05-26 19:02:01 +0100

    Added buffer 21 to resources

--- src/main/java/com/evomine/decode/PacketDeserializer.java
+++ src/main/java/com/evomine/decode/PacketDeserializer.java
@@ -324,6 +324,10 @@ public class PacketDeserializer
     {
       return buf.readShort();
     }
+    else if ( type.equals( "f64" ) )
+    {
+      return buf.readDouble();
+    }
     else if ( type.equals( "compound" ) )
     {
       return readCompound( buf );
